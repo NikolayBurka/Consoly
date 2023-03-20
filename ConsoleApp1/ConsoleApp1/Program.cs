eb565fdeb22829d3b60f862861e2b29fa92a3bfb
ИИП ТТ <kab31lab@gmail.com>
2023-03-20 09:15:09 +0200

    //Zadanie1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,21 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Задание №2 Известен рост каждого студента 212 группы.Найти средний рост студентов.
-        int j = int.Parse(Console.ReadLine());
-        int[] heights = new int[j];
-        for (int i = 0; i < j; i++)
-        {
-            heights[heights[i]] = i;
-        }
-        int totalHeight = 0;
-        foreach (int height in heights)
-        {
-            totalHeight += height;
-        }
-        Console.WriteLine(totalHeight);
-        double averageHeight = (double)totalHeight / j;
-        Console.WriteLine("Средний рост студентов: " + averageHeight);
+        //Задание №1 В массиве записана масса каждого из n предметов, загружаемых в автомобиль. Определить общую массу груза.
 
+        int n = int.Parse(Console.ReadLine());
+        int[] weights = new int[n];
+        for (int i = 0; i < n; i++)
+            weights[i] = i;
+        int totalWeight = 0;
+        foreach (int weight in weights)
+        {
+            totalWeight += weight;
+        }
+
+        Console.WriteLine("Общая масса груза: " + totalWeight);
+        Console.ReadLine();
     }
 }
